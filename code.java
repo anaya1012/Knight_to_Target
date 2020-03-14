31e3a15efba32b73a36a8c145cd6de7424986c8c
anaya1012 <56404064+anaya1012@users.noreply.github.com>
2020-03-14 15:18:01 +0530

    Update code.java

--- code.java
+++ code.java
@@ -120,9 +120,6 @@ class list
 
 public class Knight {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
